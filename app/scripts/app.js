8d689d7cbbe7dd1e88b15de37abe8d2e93841cb8
whoisandie <rjn143@gmail.com>
2016-04-26 19:13:10 -0700

    sadly done with the jquery todos application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,20 @@
 (function($, hbs) {
 
   var Utils = {
+    _s4: function() {
+      return Math.floor(Math.random() * 0x10000).toString(16); 
+    },
+
+    uuid: function() {
+      return this._s4() + this._s4() + '-' + this._s4() + '-' + this._s4() + '-' + this._s4() + this._s4() + this._s4();
+    },
+
     store: function(key, val) {
       if(arguments.length > 1) {
-        localStorage.setItem(key, JSON.stringify(val));
+        return localStorage.setItem(key, JSON.stringify(val));
       } else {
-        return JSON.parse(localStorage.getItem(key)) || [];
+        var store = localStorage.getItem(key);
+        return (store && JSON.parse(store)) || [];
       }
     }
   };
@@ -22,6 +31,12 @@
 
     events: function() {
       $('#todo-input').on('keyup', this.create.bind(this));
+      $('#todo-list')
+        .on('dblclick', '.view', this.edit.bind(this))
+        .on('focusout', '.edit', this.update.bind(this))
+        .on('keyup', '.edit', this.editKeyup.bind(this))
+        .on('change', '.toggle', this.toggle.bind(this))
+        .on('click', 'button', this.remove.bind(this));
     },
 
 
@@ -30,13 +45,12 @@
       var $input = $(e.target);
       var val = $input.val().trim();
       if(e.which === 13 && val) {
-        // Create a todo item
         this.todos.push({
-          name: val
+          id: Utils.uuid(),
+          name: val,
+          completed: false
         });
-        Utils.store('todos', this.todos);
 
-        // Reset the input
         $input.val("")
         $input.focus();
 
@@ -44,10 +58,72 @@
       }
     },
 
+    edit: function(e) {
+      var $input = $(e.target).closest('li').addClass('active').find('.edit');
+      $input.val($input.val()).focus();
+    },
+  
+    editKeyup: function(e) {
+      var val = $(e.target).val();
+      if(e.which !== 13) {
+        return;
+      } else {
+        var idx = this._index(e.target);
+        this.todos[idx].name = val;
+      }
+
+      this.render();
+    },
+
+    toggle: function(e) {
+      var idx = this._index(e.target);
+      this.todos[idx].completed = !this.todos[idx].completed;
+      
+      this.render();
+    },
+
+    update: function(e) {
+      var val = $(e.target).val();
+
+      if (!val) {
+        this.destroy(e);
+        return;
+      }
+
+      this.render();
+    },
+
+    remove: function(e) {
+      var idx = this._index(e.target);
+      this.todos.splice(idx, 1);
+      this.render();
+    },
+
+    destroy: function(e) {
+      var idx = this._index(e.target);
+      this.todos.splice(idx, 1);
+
+      this.render();
+    },
+
     render: function() {
-      console.log(this.todos);
-      var todos = this.todoTemplate(this.todos);
-      $('#todo-list').html(todos);
+      var todos = this.todos;
+      var todoTemplate = this.todoTemplate(todos);
+      $('#todo-list').html(todoTemplate);
+      $('#main').toggle(todos.length > 0);
+      Utils.store('todos', todos);
+    },
+
+    _index: function(el) {
+      var id = $(el).closest('li').data('id');
+      var todos = this.todos;
+      var i = todos.length;
+
+      while(i--) {
+        if(todos[i].id === id) {
+          return i;
+        }
+      }
     }
   };
 
